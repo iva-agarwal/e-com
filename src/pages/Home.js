eafc43821b0744b5c1d47cbfdd803a1ce8285700
Iva <iva.agarwal@gmail.com>
2023-11-05 11:13:23 +0530

    signup/login routing

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductList from '../products/ProductList'
+const Home = () => {
+  return (
+    <div>
+        <ProductList />
+    </div>
+  )
+}
+
+export default Home
